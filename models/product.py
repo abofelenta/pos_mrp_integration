4fbd5c1edb08a1d85c9de9dbabb2e2db2dfa5111
mostafa <mostafa@example.com>
2026-01-22 15:29:12 +0300

    Enable products to trigger manufacturing from POS.

--- models/product.py
+++ models/product.py
@@ -3,3 +3,7 @@ from odoo import models, fields
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
+    manufacture_from_pos = fields.Boolean(
+        string="Manufacture from POS",
+        help="If enabled, selling this product in POS will create a Manufacturing Order"
+    )
